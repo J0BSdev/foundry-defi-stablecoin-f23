199b7c65704b375689f8a5ddf7394573d4c2d150
Lovro <lovro.posel79@gmail.com>
2026-02-15 17:18:51 +0100

    update

--- test/mocks/unit/DSCEngine.t.sol
+++ test/mocks/unit/DSCEngine.t.sol
@@ -61,7 +61,10 @@ function testGetUSdValue() public{
     uint256 actualUsd = engine.getUsdValue(weth, ethAmount);
     assertEq(actualUsd, expectedUsd);
 
-}
+
+
+  
+}  
 
 
 
@@ -120,6 +123,18 @@ uint256 expectedDepositAmount = engine.getTokenAmountFromUsd(weth, collateralVal
 assertEq(totalDscMinted, expectedTotalDscMinted);
 assertEq( AMOUNT_COLLATERAL, expectedDepositAmount);
 
-
 }
+
+testCanDepositCollateralAndGetAcountInfo__MultipleCollaterals() public depositedCollateral{
+    vm.startPrank(deployerKey);
+    ERC20Mock(weth).approve(address(engine), AMOUNT_COLLATERAL);
+    engine.depositCollateral(weth, AMOUNT_COLLATERAL);
+    vm.stopPrank();
+    (uint256 totalDscMinted, uint256 collateralValueInUsd) = engine.getAccountInformation(deployerKey);
+    uint256 expectedTotalDscMinted = 0;
+    uint256 expectedDepositAmount = engine.getTokenAmountFromUsd(weth, collateralValueInUsd);
+    assertEq(totalDscMinted, expectedTotalDscMinted);
+    assertEq( AMOUNT_COLLATERAL, expectedDepositAmount);
+}
+
 }
