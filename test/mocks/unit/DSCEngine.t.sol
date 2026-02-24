37fbc883fc51002c22768ddf0fee9cbbd569ecb8
Lovro <lovro.posel79@gmail.com>
2026-02-24 21:33:47 +0100

    update

--- test/mocks/unit/DSCEngine.t.sol
+++ test/mocks/unit/DSCEngine.t.sol
@@ -5,17 +5,17 @@ pragma solidity ^0.8.20;
 import { Test } from "forge-std/Test.sol";
 import { DeployDSC } from "../../../script/DeployDSC.s.sol";
 import { DecentralizedStableCoin } from "../../../src/DecentralizedStableCoin.sol";
-import { DSCEngine } from "../../../src/DSCEngine.sol";
+import { DSCEngine } from "../../../src/DSCEngine.sol"; 
 import { HelperConfig } from "../../../script/HelperConfig.s.sol";
 import { ERC20Mock } from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 
 contract DSCEngineTest is Test {
 
-DeployDSC public deployer;
+
 DecentralizedStableCoin public dsc;
 DSCEngine public engine;
-HelperConfig public config;
+HelperConfig public helperConfig;
 
 address public ethUsdPriceFeed;
 address public weth;
