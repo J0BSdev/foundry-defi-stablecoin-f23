561652479f215e70afa57123023dbfe181b83c7e
Lovro <lovro.posel79@gmail.com>
2026-03-10 21:49:52 +0100

    update

--- test/mocks/fuzz/Handler.t.sol
+++ test/mocks/fuzz/Handler.t.sol
@@ -5,17 +5,39 @@ pragma solidity ^0.8.18;
 import {Test} from "forge-std/Test.sol";
 import {DSCEngine} from "../../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
+
+
+
 
 contract Handler is Test {
     DSCEngine dsce;
     DecentralizedStableCoin dsc;
 
+
+    ERC20Mock weth;
+    ERC20Mock wbtc;
+
+
     constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
         dsce = _dscEngine;
         dsc = _dsc;
+
+        address[] memory collateralTokens = dsce.getCollateralTokens();
+        weth = ERC20Mock(collateralTokens[0]);
+        wbtc = ERC20Mock(collateralTokens[1]);
     }
 
     function depositCollateral(address collateral, uint256 collateralSeed, uint256 amountCollateral) public {
-        dsce.depositCollateral(collateral, amountCollateral);
+        ERC20Mock collateralToken = _getCollateralFromSeed(collateralSeed);
+        dsce.depositCollateral(address(collateralToken), amountCollateral);
+    }
+
+    function _getCollateralFromSeed(uint256 collateralSeed) private view returns (ERC20Mock) {
+
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        }
+        return wbtc;
     }
 }
