c94f7e3de207343d074ae2119ba2a3b352ba82dc
Lovro <lovro.posel79@gmail.com>
2026-03-10 21:34:53 +0100

    update

--- test/mocks/fuzz/OpenInvariantTest.t.sol
+++ test/mocks/fuzz/OpenInvariantTest.t.sol
@@ -9,7 +9,7 @@ import {DSCEngine} from "../../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../../script/HelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import {Handler} from "./Handler.t.sol";
 
 
 
@@ -20,14 +20,16 @@ contract OpenInvariantTest is StdInvariant, Test{
     DecentralizedStableCoin dsc;
     address weth;
     address wbtc;
-
+    Handler handler;
 
 
 function setUp() external {
 deployer = new DeployDSC();
 (dsc, dsce, config) = deployer.run();
 (, , weth, wbtc,) = config.activeNetworkConfig();
-targetContract(address(dsce));
+handler = new Handler(dsce, dsc);
+targetContract(address(handler));
+//targetContract(address(dsce));
 
 }
 
