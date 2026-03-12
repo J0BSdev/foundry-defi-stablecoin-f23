8f0c1f7e7dca02479f8b95fc296d3ebee10a2b14
Lovro <lovro.posel79@gmail.com>
2026-03-12 21:29:43 +0100

    update

--- test/mocks/fuzz/Handler.t.sol
+++ test/mocks/fuzz/Handler.t.sol
@@ -31,7 +31,18 @@ contract Handler is Test {
     }
 
 function mintDsc(uint256 amount) public{
- amount = bound(amount, 1, MAX_DEPOSIT_SIZE);
+vm.startPrank(msg.sender);
+(uint256 totalDscMinted, uint256 collateralValueInUSD) = dsce.getAccountInformation(msg.sender);
+int256 maxDscToMint = (int256(collateralValueInUSD) / 2) - int256(totalDscMinted);
+if (maxDscToMint < 0) {
+    return;
+}
+
+amount = bound(amount, 0, uint256(maxDscToMint));
+if (amount == 0) {
+    return;
+}
+
 vm.startPrank(msg.sender);
 dsc.mint(msg.sender, amount);
 vm.stopPrank();
