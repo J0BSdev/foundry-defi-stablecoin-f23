8e4afee9b0eb79ac3191025e0cbc636595fba5e3
Lovro <lovro.posel79@gmail.com>
2026-08-31 20:55:50 +0200

    update

---
+++ DSCEnginePractice/DSCEnginePractice.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.20;
+
+import{DecentralizedStableCoin} from
+import{}
+
+contract DSCEnginePractice is ReentrancyGuard{
+
+    function depositCollateral()public payable
+
+    depositCollateral(tokenAddress)
+}
---
+++ DSCEnginePractice/README.md
@@ -0,0 +1,5 @@
+this is practice contract.
+
+this protoclo is used to   
+
+1. Users have to be capable of depositing collateral and minting DSC
