2b56aeef5e676d651a2753327b1e921b8b805d91
Lovro <lovro.posel79@gmail.com>
2026-08-31 21:34:54 +0200

    update

--- DSCEnginePractice/DSCEnginePractice.sol
+++ DSCEnginePractice/DSCEnginePractice.sol
@@ -2,12 +2,22 @@
 
 pragma solidity ^0.8.20;
 
-import{DecentralizedStableCoin} from
-import{}
+import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 
-contract DSCEnginePractice is ReentrancyGuard{
+
+contract DSCEnginePractice {
+
+    IERC20 public weth;
+    DecentralizedStableCoin public dsc;
+
+    mapping(address => uint256) public collateralDeposited;
+    mapping(address => uint256) public debtMInted;
+
+}
+
 
     function depositCollateral()public payable
 
     depositCollateral(tokenAddress)
+}
 }
