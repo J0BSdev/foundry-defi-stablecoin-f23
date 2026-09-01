adf982048a81a2922e449ad7807caadd5be3eeb4
Lovro <lovro.posel79@gmail.com>
2026-09-01 18:47:29 +0200

    update

--- DSCEnginePractice/DSCEnginePractice.sol
+++ DSCEnginePractice/DSCEnginePractice.sol
@@ -5,6 +5,9 @@ pragma solidity ^0.8.20;
 import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
 
 
+error DSC__EngineNeedsMoreThanZero();
+
+
 contract DSCEnginePractice {
 
     IERC20 public weth;
@@ -16,7 +19,13 @@ contract DSCEnginePractice {
 
     function depositCollateral(uint256 amount)external{
         weth.tranferFrom(msg.sender,address(this),amount);
+       //we have to transfer collateral from user to protocol
         collateralDeposited[msg.sender]+= amount;
+        //we have to confirm users deposit
+       if (amount == 0 )
+       revert DSC__EngineNeedsMoreThanZero();  
+
+    }
     
     
         
