0534e8d08281a4fd260535c4916610d9442a95a1
Lovro <lovro.posel79@gmail.com>
2026-09-02 18:06:21 +0200

    update

--- DSCEnginePractice/DSCEnginePractice.sol
+++ DSCEnginePractice/DSCEnginePractice.sol
@@ -2,29 +2,27 @@
 
 pragma solidity ^0.8.20;
 
-import {DecentralizedStableCoin} from "./DecentralizedStableCoin.sol";
+import {ERC20} from "@openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 
+contract DSCEnginePractice is ERC20{
 
-error DSC__EngineNeedsMoreThanZero();
+error DSC__EngineNeedsMoreThanZero(); 
 
-
-contract DSCEnginePractice {
-
-    IERC20 public weth;
-    DecentralizedStableCoin public dsc;
 
     mapping(address => uint256) public collateralDeposited;
     mapping(address => uint256) public debtMInted;
 
+    constructor()ERC20("DSCEngine" , "DSC"){}
 
-    function depositCollateral(uint256 amount)external{
+    function depositCollateral(uint256 amount)external{ 
+         if (amount == 0 )
+       revert DSC__EngineNeedsMoreThanZero();  
         weth.tranferFrom(msg.sender,address(this),amount);
+        bool success = weth.tranferFrom(msg.sender,address(this),amount);
        //we have to transfer collateral from user to protocol
         collateralDeposited[msg.sender]+= amount;
         //we have to confirm users deposit
-       if (amount == 0 )
-       revert DSC__EngineNeedsMoreThanZero();  
-
+     
     }
     
     
